f8c311b14ec98e2b793e382f855c4600f00e6d7d
Armin <sharpusenetbackup@aoonk.nl>
2018-07-18 20:45:53 +0200

    testing with getting qr code

--- test.py
+++ test.py
@@ -3,15 +3,25 @@ from picamera import PiCamera
 import time
 from picamera.array import PiYUVArray
 import cv2
+import pyzbar.pyzbar as pyzbar
+
 
 camera = PiCamera()
 camera.resolution = (640, 480)
 rawCapture = PiYUVArray(camera, size=camera.resolution)
 
-time.sleep(0.1)
+camera.start_preview()
+time.sleep(2.0)
 
 camera.capture(rawCapture, format="yuv")
-image = rawCapture.array
-print(str(image.shape))
-cv2.imwrite('test_je.png', image[:480, :640, 0])
+camera.stop_preview()
 
+image = rawCapture.array
+image = image[:480, :640, 0]
+cv2.imwrite('test_je.png', image)
+
+decodedObjects = pyzbar.decode(image)
+
+for obj in decodedObjects:
+    print('Type : ' + str(obj.type))
+    print('Data : ' + str(obj.data))
