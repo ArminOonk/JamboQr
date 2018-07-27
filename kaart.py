239f11ae0c428120c42f0cd524af5482d217a056
Armin Oonk <Armin@berkelaarmrt.com>
2018-07-27 20:49:20 +0200

    Nu met antwoorden

--- kaart.py
+++ kaart.py
@@ -15,22 +15,24 @@ print('Number of unique teams: ' + str(len(list(set(teams)))))
 with open('teamnamen sorted.txt', 'w') as f:
     f.write('\n'.join(sorted(set(teams))))
 
-questions = []
-d = dict()
-d['q'] = 'Wie is de teamleider van TEAT?'
-d['a'] = 'Donald duck'
-d['b'] = 'Michiel'
-d['c'] = 'Jan Smit'
-d['d'] = 'Wybo'
-questions.append(d)
-
-e = dict()
-e['q'] = 'Wie is de teamleider van Aqua?'
-e['a'] = 'Donald duck'
-e['b'] = 'Neptunus'
-e['c'] = 'Jan Smit'
-e['d'] = 'Pien'
-questions.append(e)
-
-with open('question.txt', 'w') as f:
-    f.write(json.dumps(questions, sort_keys=True, indent=4))
+# questions = []
+# d = dict()
+# d['q'] = 'Wie is de teamleider van TEAT?'
+# d['a'] = 'Donald duck'
+# d['b'] = 'Michiel'
+# d['c'] = 'Jan Smit'
+# d['d'] = 'Wybo'
+# d['ans'] = 'b'
+# questions.append(d)
+#
+# e = dict()
+# e['q'] = 'Wie is de teamleider van Aqua?'
+# e['a'] = 'Donald duck'
+# e['b'] = 'Pien'
+# e['c'] = 'Jan Smit'
+# e['d'] = 'Neptunes'
+# e['ans'] = 'd'
+# questions.append(e)
+#
+# with open('question.txt', 'w') as f:
+#     f.write(json.dumps(questions, sort_keys=True, indent=4))
